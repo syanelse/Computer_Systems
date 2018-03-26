6a132518082ece7e36c9881da6313198486d2d51
Маша Саранцева <syanelse@gmail.com>
2018-03-27 00:38:06 +0300

    Lab 1

---
+++ Lab_1/Part_1/Lab_1_1/Lab_1_1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯabcdefghijklmnopqrstuvwxyzабвгґдеєжзиіїйклмнопрстуфхцчшщьюя0123456789 '\\\"!@#№$%^&*()-_=+[]{}/|,.<>?~\n";
+            Dictionary<char, int> chars = new Dictionary<char, int>();
+            Console.Write("Путь к файлу: ");
+            string str = @Console.ReadLine();
+
+            StreamReader sr = File.OpenText(str);
+
+            foreach(char c in alphabet)
+            {
+                chars.Add(c, 0);
+            }
+
+            double amountOfChars = CountChars(chars, sr);
+            Dictionary<char, double> charFrequency;
+            charFrequency = CountFrequency(chars, amountOfChars, alphabet);
+
+            Console.WriteLine("Частоты появления символов в тексте:");
+            foreach (char c in charFrequency.Keys)
+            {
+
+                if (c != '\n')
+                {
+                    Console.Write(c);
+                    Console.WriteLine(": " + charFrequency[c]);
+                }
+                else
+                {
+                    Console.WriteLine("Переносов строки: " + charFrequency[c]);
+                }
+
+            }
+            double entrophy = CountEntrophy(charFrequency, alphabet);
+            Console.WriteLine("Энтропия: " + entrophy);
+            Console.WriteLine("Количество информации: " + entrophy * amountOfChars + " b (" + (entrophy * amountOfChars) / 8 + " B)");
+            Console.WriteLine("Длинна файла: " + sr.BaseStream.Length + " B");
+
+            Console.ReadKey();
+        }
+        static int CountChars(Dictionary<char, int> chars, StreamReader sr)
+        {
+            int counter = 0;
+            string str;
+            while (!sr.EndOfStream)
+            {
+                str = sr.ReadLine();
+                foreach (char c in str)
+                {
+                    try
+                    {
+                        chars[c]++;
+                    }
+                    catch
+                    {
+                        chars.Add(c, 1);
+                    }
+                }
+                counter += str.Length;
+                chars['\n']++;
+            }
+            return counter;
+        }
+        static Dictionary<char, double> CountFrequency(Dictionary<char, int> chars, double amountOfChars, string alphabet)
+        {
+            Dictionary<char, double> frequency = new Dictionary<char, double>();
+            foreach (char c in alphabet)
+            {
+                frequency.Add(c, chars[c] / amountOfChars);
+            }
+
+            return frequency;
+        }
+        static double CountEntrophy(Dictionary<char, double> chars, string alef)
+        {
+            double entrophy = 0.0;
+            foreach (char c in alef)
+            {
+                if (chars[c] != 0)
+                {
+                    entrophy -= chars[c] * Math.Log(chars[c], 2);
+                }
+            }
+            return entrophy;
+        }
+    }
+}
---
+++ Lab_1/Part_2/Lab_1_2/Lab_1_2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using ilon = System.Int32;
+using System.Threading.Tasks;
+
+namespace Lab_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                flag = false;
+                Console.Write("Путь к исходному файлу: ");
+                string strIn = @"C:\Users\Seashell\Desktop\Универ\CS\Lab_1\Part_2\1.txt";//@Console.ReadLine();
+                Console.Write("Путь к закодированому файлу: ");
+                string strOut = @"C:\Users\Seashell\Desktop\Универ\CS\Lab_1\Part_2\2.txt";//@Console.ReadLine();
+                ConvertToBase64(strIn, strOut);
+                Console.ReadKey();
+            }
+        }
+        static void ConvertToBase64(string source, string destination)
+        {
+            using (File.Create(destination))
+            { }
+            BinaryReader r = new BinaryReader(File.Open(source, FileMode.Open));
+            int[] converted = new int[4];
+            int k = -1;
+            Int64 threeOctets = 0;
+            // kak tebe takoe,
+            ilon mask /*???*/
+                = 16515072; //111111 000000 000000 000000
+
+            while (r.BaseStream.Position != r.BaseStream.Length)
+            {
+                threeOctets = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    try
+                    {
+                        threeOctets |= r.ReadByte();
+                        threeOctets <<= 8;
+                    }
+                    catch
+                    {
+                        k = i;
+                        while (i < 3)
+                        {
+                            threeOctets <<= 8;
+                            i++;
+                        }
+                    }
+                }
+                threeOctets >>= 8;
+
+                //mask = 16515072; //111111 000000 000000 000000
+                mask = 63; // 000000 000000 000000 111111
+
+                for (int i = 3; i >= 0; i--)
+                {
+                    converted[i] = ((int)threeOctets & mask);
+                    threeOctets >>= 6;
+                }
+                if (k != -1)
+                {
+                    k++;
+                    while (k < 4)
+                    {
+                        converted[k] = 64;
+                        k++;
+                    }
+                }
+                WriteFile(destination, converted);
+
+               
+            } r.Close();
+        }
+
+        static void WriteFile(string path, int[] converted)
+        {
+            StreamWriter writer = new StreamWriter(File.Open(path, FileMode.Append), Encoding.ASCII);
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+            for (int i = 0; i < 4; i++)
+            {
+                writer.Write(alphabet[converted[i]]);
+            }
+            writer.Close();
+
+        }
+    }
+}
+
